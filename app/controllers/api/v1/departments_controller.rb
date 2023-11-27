f26ee199a37d25c07377ecd9037b477d3685a603
forLoop94 <charlesuwem94@gmail.com>
2023-11-27 10:00:48 +0100

    Add CRUD actions to departments controller and update routes.rb

---
+++ app/controllers/api/v1/departments_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::DepartmentsController < ApplicationController
+  def index
+  @departments = department.all
+  render json: @departments
+  end
+
+  def show
+  @department = Department.find(params[:id])
+  render json: @department
+  end
+
+  def create
+    @department = Department.new(dept_params)
+
+    if @department.save
+      render json: { department: @department, message: "Department created succesfully" }, status: :created
+    else
+      render json: @department.errors, message: "Department could not be created", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def dept_params
+    params.require(:department).permit(:name, :school_id)
+  end
+end
