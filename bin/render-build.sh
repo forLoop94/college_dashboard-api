7c76f1b458d3dda0e4ab56985b9a9fd53f9543b3
forLoop94 <charlesuwem94@gmail.com>
2024-01-14 11:42:29 +0100

    Modify based on new render doc

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,22 @@
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
+
+# # bundle install
+# # bundle exec rails assets:precompile
+# # bundle exec rails assets:clean
+# # bundle exec rails db:migrate
+
+# bundle install
+# ./bin/rails assets:precompile
+# ./bin/rails assets:clean
+# ./bin/rails db:migrate
+
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+./bin/rails assets:precompile
+./bin/rails assets:clean
+./bin/rails db:migrate
